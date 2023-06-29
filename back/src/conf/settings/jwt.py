2da9a71b32ce6296c2a9f03a474321cd568c4e74
gravity <gravity4849@gmail.com>
2023-06-29 18:14:21 +0300

    add fixtures to docker file

--- back/src/conf/settings/jwt.py
+++ back/src/conf/settings/jwt.py
@@ -2,28 +2,23 @@ import os
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1, minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=int(os.environ['ACCESS_TOKEN_LIFETIME'])),
+    'REFRESH_TOKEN_LIFETIME': timedelta(seconds=int(os.environ['REFRESH_TOKEN_LIFETIME'])),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
-
     'ALGORITHM': 'HS256',
     'SIGNING_KEY': os.environ['DJANGO_SECRET_KEY'],
     'VERIFYING_KEY': None,
     'AUDIENCE': None,
     'ISSUER': None,
-
     'AUTH_HEADER_TYPES': ('Bearer',),
     'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
     'USER_ID_FIELD': 'id',
     'USER_ID_CLAIM': 'user_id',
-
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
     'TOKEN_TYPE_CLAIM': 'token_type',
-
     'JTI_CLAIM': 'jti',
-
     'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
     'SLIDING_TOKEN_LIFETIME': timedelta(minutes=1),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(minutes=5),
