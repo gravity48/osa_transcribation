53f1e8177a0bb9b60df200be6aa07dc078ffae9c
gravity <gravity4849@gmail.com>
2023-06-30 15:05:25 +0300

    add navbar

---
+++ front/pages/connections/_id/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>Show Connection Detail {{this.$route.params.id}}</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: "index",
+  mounted() {
+
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
